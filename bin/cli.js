993be91d4c6fc504a64891866a27628018e1efe4
Pete Diemert <pete@playup.com>
2011-12-16 17:39:41 +1100

    latest

---
+++ bin/cli.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+;(function ()
+    { // wrapper in case we're in module_context mode
+
+// windows: running "ssa blah" in this folder will invoke WSH, not node.
+        if (typeof WScript !== "undefined")
+            {
+                WScript.echo("sally does not work when run\n" + "with the Windows Scripting Host\n\n" + "'cd' to a different directory,\n" + "or type 'ssa.cmd <args>',\n" + "or type 'node ssa <args>'.");
+                WScript.quit(1);
+                return;
+            }
+
+        var consts = require('../lib/const.js');
+        var program = require('../modules/commander.js');
+        var childp = require('child_process');
+        var files = [];
+        var host;
+        var port;
+
+        program.usage('[options] [filespec]')
+            .description('Start a slave load generator or initiates a load test\nas the master if filespec specifies a .JS load test')
+            .option('-m, --master [host]', 'If a slave, the master host [' + consts.defaultMasterHost + ']')
+            .option('-up, --uport <n>', 'If a slave, the slave to master port [' + consts.defaultWorkerOutPort + ']')
+            .option('-dp, --dport <n>', 'If a slave, the master to slave port [' + consts.defaultWorkerInPort + ']')
+            .option('-d, --debug', 'Passes --debug-brk to node]')
+            .parse(process.argv);
+
+        var host = program.master || consts.defaultMasterHost;
+        var uport = program.uport || consts.defaultWorkerOutPort;
+        var dport = program.dport || consts.defaultWorkerInPort;
+        var debug = program.debug;
+
+        function puts(msg)
+            {
+                process.stdout.write(msg);
+            }
+        function putl(msg)
+            {
+                process.stdout.write(msg + '\n');
+            }
+
+        function spawn(file, cb)
+            {
+                var prc = childp.spawn(process.argv[0], debug ? ['--debug-brk',file] : [file]);
+
+                var out = '';
+                prc.stdout.on("data", function(data)
+                {
+                    puts(data.toString());
+                    out += data;
+                });
+                prc.stderr.on('data', function(data)
+                {
+                    puts(data.toString());
+                    out += data;
+                });
+
+                var cbT = cb;
+
+                prc.on("exit", function(code)
+                {
+                    if (cbT)
+                        cbT(code, out);
+                });
+            }
+
+        if (program.args.length > 0)
+            {
+                // We are master
+
+                if (program.master)
+                    {
+                        out(program);
+                        out('Error: Can not specify a load test (i.e. be a master) and specify slave options as well');
+                        process.exit(1);
+                        return;
+                    }
+
+                files = program.args.slice();
+
+                putl('Running as the master, up port ' + uport + ', down port ' + dport);
+
+                files.forEach(function(f)
+                {
+                   spawn(f, function(code, output)
+                   {
+                      console.log('done');
+                   });
+                });
+            }
+        else
+            {
+                putl('Running as a slave to ' + host + ', up port ' + uport + ', down port ' + dport);
+
+                // We are slave
+                var wkr = require('../lib/worker.js');
+
+                wkr.start(host, uport, dport);
+            }
+
+    })();
